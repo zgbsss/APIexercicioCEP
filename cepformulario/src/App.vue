0fa85befd6eaacd9b372bb06e8c135eb6745ebbb
zgbsss <99218597+zgbsss@users.noreply.github.com>
2022-09-21 09:16:48 -0300

    Add files via upload

--- cepformulario/src/App.vue
+++ cepformulario/src/App.vue
@@ -4,32 +4,50 @@ export default {
   data() {
     return {
       cep: "",
+      endereco: {},
     };
   },
   methods: {
     async buscar() {
-      const url = `https://www.cepaberto.com/api/v3/cep?cep=01001000`;
-      this.resultado = axios.get(url, {
-        headers: {
-          Authorization: "Token token=3f6dd21bfd46e236e2ba50b948aedf94",
-        },
-      });
+      const url = `https:/viacep.com.br/ws/${this.cep}/json`;
+      const res = await axios.get(url);
+      this.endereco = res.data;
     },
   },
 };
 </script>
+
 <template>
   <div class="container">
     <h1>Busca CEP</h1>
-    <form @submit.prevent="search">
-      <input type="text" placeholder="CEP" v-model="cep" />
+    <form @submit.prevent="buscar">
+      <input type="text" v-model="cep" placeholder="CEP" />
       <button type="submit">Buscar</button>
+    </form>
+    <form class="end">
+      <label for="rua">Rua</label>
+      <input type="text" v-model="endereco.logradouro" />
+      <label for="rua">Bairro</label>
+      <input type="text" v-model="endereco.bairro" />
+      <label for="rua">DDD</label>
+      <input type="text" v-model="endereco.ddd" />
+      <label for="rua">Cidade</label>
+      <input type="text" v-model="endereco.localidade" />
+      <label for="rua">UF</label>
+      <input type="text" v-model="endereco.uf" />
+      <label for="rua">IBGE</label>
+      <input type="text" v-model="endereco.ibge" />
     </form>
   </div>
 </template>
+
 <style>
 .container {
   background-color: #fcb772;
   padding: 10px;
+}
+.end {
+  display: flex;
+  flex-direction: column;
 }
 </style>
